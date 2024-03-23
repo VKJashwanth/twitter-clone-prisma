8d21dd35bd6c3a2660e9ba9b1e50a5f16e4c0280
Jashwanth <vk.vkjashwanth@gmail.com>
2024-03-22 17:33:25 -0700

    Initial commit from Create Next App

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import PostFeed from "@/components/posts/PostFeed"
+import Header from "@/components/Header"
+import Form from "@/components/Form"
+import Head from "next/head"
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Twitter</title>
+      </Head>
+      <Header label="Home" />
+      <Form placeholder="What's happening?" />
+      <PostFeed />
+    </>
+  )
+}
